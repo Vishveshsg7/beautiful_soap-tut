e584a0324ce4cc4b4470a95521e0f112128910b2
Vishvesh_7 <103508415+Vishveshsg7@users.noreply.github.com>
2024-07-14 00:00:08 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+with open('home.html', 'r') as html_file:
+    content = html_file.read()
+    #print(content)
+    soup = BeautifulSoup(content, 'lxml')
+    #print(soup.prettify())
+    course_cards = soup.find_all('div', class_='card')
+    for course in course_cards:
+        course_name=course.h5.text
+        course_price=course.a.text.split()[-1]
+        print(f'{course_name} costs {course_price}')
+        #print(course_price)
